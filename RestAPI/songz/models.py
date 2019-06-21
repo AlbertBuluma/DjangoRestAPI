0fcbb3b17b31d84c31888d1df2a15d437ab0a52b
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 11:37:34 +0300

    Define model attributes.

--- RestAPI/songz/models.py
+++ RestAPI/songz/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Songz(models.Model):
+    title = models.CharField(max_length=15)
+    artist = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.artist

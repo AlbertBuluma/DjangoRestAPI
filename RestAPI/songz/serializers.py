c9bc7184d1a2e7c11df9636375d941883b158ffb
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 12:13:46 +0300

    Add serializer model and field attributes.

---
+++ RestAPI/songz/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Songz
+
+
+
+class SongzSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Songz
+        fields = ('title', 'artist')
+
+
+
+

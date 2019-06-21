54058beb4fb6e5cd3dbbc9b029238e8775019aa2
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 15:41:53 +0300

    Import viewsets class.

--- RestAPI/songz/views.py
+++ RestAPI/songz/views.py
@@ -1,8 +1,9 @@
 from . models import Songz
 from . serializers import SongzSerializer
-from rest_framework import generics
+from rest_framework import generics, viewsets
 # Create your views here.
 
 class ListSongzView(generics.ListAPIView):
+# class ListSongzView(viewsets.ModelViewSet):
     queryset = Songz.objects.all()
     serializer_class = SongzSerializer

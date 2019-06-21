498607c1466cdce199a7e3fc664096e8a7b31e7b
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 15:04:30 +0300

    Add song creation test.

--- RestAPI/songz/tests.py
+++ RestAPI/songz/tests.py
@@ -5,3 +5,30 @@ from rest_framework.test import APIClient, APITestCase
 from . serializers import SongzSerializer
 # Create your tests here.
 
+
+class BaseViewTest(APITestCase):
+
+    client = APIClient
+
+    @staticmethod
+    def createsong(title='', song=''):
+        if title!='' and song!='':
+            Songz.objects.create(title=title, song=song)
+
+    def setUp(self):
+        self.createsong('Speeding', 'Omarion')
+        self.createsong('Duro', 'Tekno')
+        self.createsong('Malaika', 'Vinka')
+        self.createsong('Taken', 'Trey Songz')
+
+
+class GetAllSongzTest(BaseViewTest):
+
+    def test_get_all_songz(self):
+        response = self.client.get(
+            reverse('songz-all', kwargs={'version':'v1'}))
+
+        expected = Songz.objects.all()
+        serialized = SongzSerializer(expected, many=True)
+        self.assertEqual(response.data, serialized.data)
+        self.assertEqual(response.status, status.HTTP_200_OK)

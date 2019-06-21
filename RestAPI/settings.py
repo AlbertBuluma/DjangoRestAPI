7e5ccbe1934f0e2f26b9cc4244ddf40fced4a3b9
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 17:03:04 +0300

    Configuring Travis TestDB.

--- RestAPI/settings.py
+++ RestAPI/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'RestAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_api',
+        'NAME': 'testdb',
         'USER': 'postgres',
         'PASSWORD': 'albert256',
         'HOST':'localhost',

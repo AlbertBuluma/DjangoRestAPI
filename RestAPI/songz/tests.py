8d8e37cda41c20d62a7c7a399ef5cfb91dea9f60
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 11:45:52 +0300

    Import test dependency classes.

--- RestAPI/songz/tests.py
+++ RestAPI/songz/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
+from django.urls import reverse
+from . models import Songz
+from rest_framework.views import status
+from rest_framework.test import APIClient, APITestCase
+from . serializers import SongzSerializer
 # Create your tests here.
+

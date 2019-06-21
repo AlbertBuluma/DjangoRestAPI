8f6c31d399ac1b35e676fd4fb70d567279d9f003
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 14:56:23 +0300

    Add songs list view.

--- RestAPI/songz/views.py
+++ RestAPI/songz/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from . models import Songz
+from . serializers import SongzSerializer
+from rest_framework import generics
 # Create your views here.
+
+class ListSongzView(generics.ListAPIView):
+    queryset = Songz.objects.all()
+    serializer_class = SongzSerializer

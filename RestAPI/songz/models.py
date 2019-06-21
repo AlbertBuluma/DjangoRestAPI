4e65625ec8d58f17123acb2294a0f106148b01e1
AlbertBuluma <albert.buluma1@gmail.com>
2019-06-21 12:20:04 +0300

    Add null attribute to model field.

--- RestAPI/songz/models.py
+++ RestAPI/songz/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Songz(models.Model):
-    title = models.CharField(max_length=15)
-    artist = models.CharField(max_length=15)
+    title = models.CharField(max_length=15, null=False)
+    artist = models.CharField(max_length=15, null=False)
 
     def __str__(self):
         return f'{self.artist} - {self.title}'
